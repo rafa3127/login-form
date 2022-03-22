9b33c12a611705709b55579e0c47ac02498f6ebd
Rafael Correa <rafaelc3127@gmail.com>
2022-03-22 13:18:25 -0400

    login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'login-form';
+  form: FormGroup 
+
+  constructor(
+    public fb: FormBuilder
+  ){
+    this.form = this.createForm()
+  }
+  createForm():FormGroup{
+    var form:any = this.fb.group({
+      email: ['',Validators.compose([
+        Validators.required,
+        Validators.email
+      ])],
+      password: ['',Validators.compose([
+        Validators.required
+      ])],
+      remember: [false]
+    })
+    return form
+  }
+
+  logIn(){
+    //Here must be the service function that controls the logIn process
+    return this.form.value
+  }
 }
